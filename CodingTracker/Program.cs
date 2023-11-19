60824fbea32f4d37ab22c13e9fe982f987c0bcaa
martinagustin <g4rciamartin@gmail.com>
2023-11-19 17:11:57 +0100

    se agregao mas contenido a line 2

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, c# academy!"); Console.WriteLine("Hello, c# academy!"); Console.WriteLine("Hello, c# academy!"); Console.WriteLine("Hello, c# academy!"); Console.WriteLine("Hello, c# academy!"); Console.WriteLine("Hello, c# academy!");
+Console.WriteLine("Hello, c# academy!"); Console.WriteLine("Hello, c# academy!"); Console.WriteLine("Hello, c# academy!"); Console.WriteLine("Hello, c# academy!"); Console.WriteLine("Hello, c# academy!"); Console.WriteLine("Hello, c# academy!");//many
